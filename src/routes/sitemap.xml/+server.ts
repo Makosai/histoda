a43b1eda66bdafe49ddcadde9e7a37158fafbbeb
Makosai <kristopher.ali.dev@gmail.com>
2026-06-21 11:18:00 -0400

    clean up sitemap spaces

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -1,8 +1,8 @@
 import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async ({ url }) => {
-	const origin = url.hostname === 'localhost' || url.hostname === '127.0.0.1' 
-		? url.origin 
+	const origin = url.hostname === 'localhost' || url.hostname === '127.0.0.1'
+		? url.origin
 		: 'https://histoda.com';
 	const body = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
